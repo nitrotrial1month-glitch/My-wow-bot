99569703401c09a656eae403c3e2459abc8c754a
nitrotrial1month-glitch <nitrotrial1month@gmail.com>
2026-02-10 10:11:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,12 +49,6 @@ server_data = {
         "color": 0xff0000
     }
 }
-class MyBot(commands.Bot):
-    def __init__(self):
-        intents = discord.Intents.default()
-        intents.message_content = True
-        intents.members = True 
-        super().__init__(command_prefix="!", intents=intents)
     async def setup_hook(self):
         # বাটনগুলো মেমোরিতে সেভ করা (যাতে Interaction Failed না আসে)
         try:
